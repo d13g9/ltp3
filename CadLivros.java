093e88e14562d200bbd24e37f0a6709e735faafd
d13g9 <dlbtar@gmail.com>
2017-03-14 20:39:33 -0300

    classe Library
    representa o repositório

---
+++ CadLivros.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class CadLivros {
+
+	static public ArrayList<Object> booksLibrary = 
+									new ArrayList<Object>();
+	
+	static public void addBooks(Object Books){
+		booksLibrary.add(Books);		
+	}
+	static public object searchById(int id){
+		for(Object book : booksLibrary){
+			if(book.)
+		}
+		
+	}
+}
