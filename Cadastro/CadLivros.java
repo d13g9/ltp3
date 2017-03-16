4d4d58c064d2a374cf148ef41c6ab7ea28510eb5
d13g9 <dlbtar@gmail.com>
2017-03-15 21:55:32 -0300

    Add files via upload

---
+++ Cadastro/CadLivros.java
@@ -0,0 +1,37 @@
+package Cadastro;
+import java.util.ArrayList;
+import Dados.*;
+
+public class CadLivros {
+
+	static public ArrayList<Livro> booksLibrary = 
+									new ArrayList<Livro>();
+	//add book
+	static public void addBooks(Livro book){
+		booksLibrary.add(book);		
+	}
+	//procura por ID
+	static public Livro searchById(int id){
+		for(Livro book : booksLibrary){
+			if(book.getId() == id)
+				return book;
+		}
+		return null;
+		
+	}
+	//remove livro
+	static public void removeBook(Livro books){
+		booksLibrary.remove(books);		
+	}
+	
+	static public ArrayList<Livro> searchByName(String name){
+	    ArrayList<Livro> listfilter = new ArrayList<Livro>();
+		
+		for(Livro book : booksLibrary){
+			if(book.getTitle().contains(name))
+				listfilter.add(book);
+		}
+		return listfilter;
+		
+	}
+}
---
+++ Dados/Livro.java
@@ -0,0 +1,60 @@
+package Dados;
+
+import java.util.GregorianCalendar;
+
+public class Livro {
+	
+	private int id;
+	private String title,author;
+	private GregorianCalendar date_record;
+	
+	public Livro(int id,String title, String author, 
+									GregorianCalendar date_record)
+	{
+		this.setId(id);
+		this.setTitle(title);
+		this.setAuthor(author);
+		this.setDate_record(date_record);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public GregorianCalendar getDate_record() {
+		return date_record;
+	}
+
+	public void setDate_record(GregorianCalendar date_record) {
+		this.date_record = date_record;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String toString()
+	{
+		return "Id is "+this.id + "\n" +" Title is "+this.title +
+				" \n Author is "+this.author +" \n Date of Record "+
+				this.date_record;
+		
+				
+	}
+
+}
